473d2f40f7efdb9956d3c0edfb172a675141db7a
Mohamed El Karmoudi <karmoudiMohamed@gmail.com>
2017-12-01 21:37:16 +0100

    Ajout des inputs à la page inscription

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # PictionnaryLp
+
+http://miageprojet2.unice.fr/index.php?title=User:Max/LPSIL_IDSE_-_Web_Multim%C3%A9dia_%2F%2F_Web_S%C3%A9mantique/Partie_1:_Application_Pictionnary&highlight=pictionnary
+
+// Questions page inscription :
+
+- c'est quoi les attributs action et method ?
+	L'attribut "action" permet d'indiquer la page qui se chargera du traitement des données inscrit par l'utilisateur.
+	L'attribut "method" permet d'indiquer la facon dont les données seront envoyé. 
+
+- qu'y a-t-il d'autre comme possiblité que post pour l'attribut method ?
+	Il existe la "method" POST, GET, PUT 
+
+- quelle est la différence entre les attributs name et id ? 
+    "id" permet de d'identifier l'input dans le DOM.
+    "name" permet de nommer le champs. La valueur de cette input sera accessible grace au "name" depuis php par exemple. Elle permet d'identifier le champs.
+ 
+- c'est lequel qui doit être égal à l'attribut for du label ? 
+	C'est l'identifiant qui doit être égal à l'attribut for du label.
+
--- inscription.php
+++ inscription.php
@@ -1,53 +1,94 @@
 <!DOCTYPE html>  
 <html>  
-<head>  
-    <meta charset=utf-8 />  
-    <title>Pictionnary - Inscription</title>  
-</head>  
-<body>  
-  
-<h2>Inscrivez-vous</h2>  
-<form class="inscription" action="req_inscription.php" method="post" name="inscription">  
-    <!-- c'est quoi les attributs action et method ?
-        L'attribut "action" permet d'indiquer la page qui se chargera du traitement des données inscrit par l'utilisateur.
-        L'attribut "method" permet d'indiquer la facon dont les données seront envoyé. 
-    -->  
-    <!-- qu'y a-t-il d'autre comme possiblité que post pour l'attribut method ?
-        Il existe la "method" POST, GET, PUT 
-     -->  
-    <span class="required_notification">Les champs obligatoires sont indiqués par *</span>  
-    <ul>  
-        </li>  
-        <li>  
-            <label for="email">E-mail :</label>  
-            <input type="email" name="email" id="email" autofocus required/>  
-            <!-- ajouter à input l'attribut qui lui donne le focus automatiquement -->  
-            <!-- ajouter à input l'attribut qui dit que c'est un champs obligatoire -->  
-            <!-- quelle est la différence entre les attributs name et id ? 
-                "id" permet de d'identifier l'input dans le DOM.
-                "name" permet de nommer le champs. La valueur de cette input sera accessible grace au "name" depuis php par exemple. Elle permet d'identifier le champs.
-            -->  
-            <!-- c'est lequel qui doit être égal à l'attribut for du label ? 
-                C'est l'identifiant qui doit être égal à l'attribut for du label.
-            -->   
-            <span class="form_hint">Format attendu "name@something.com"</span>  
-        </li>  
-        <li>  
-            <label for="prenom">Prénom :</label>  
-            <input type="text" name="prenom" id="prenom" required placeholder="Votre prénom"/>  
-            <!-- ajouter à input l'attribut qui dit que c'est un champs obligatoire -->  
-            <!-- ajouter à input l'attribut qui donne une indication grisée (placeholder) -->  
-        </li>  
+    <head>  
+        <meta charset=utf-8 />  
+        <title>Pictionnary - Inscription</title>  
+    </head>  
+    
+    <body>        
+        <h2>Inscrivez-vous</h2>  
         
-        <li>  
-            <label for="mdp">Mot de passe :</label>  
-            <input type="password" name="mdp" id="mdp" required placeholder="Votre prénom"/>    
-        </li> 
+        <form class="inscription" action="req_inscription.php" method="post" name="inscription">  
+            <span class="required_notification">Les champs obligatoires sont indiqués par *</span>  
+            
+            <ul>  
+                <li>  
+                    <label for="email">E-mail :</label>  
+                    <input type="email" name="email" id="email" autofocus required/>  
+                    <span class="form_hint">Format attendu "name@something.com"</span>  
+                </li>  
+                
+                <li>  
+                    <label for="mdp">Mot de passe :</label>  
+                    <input type="password" name="mdp" id="mdp" placeholder="Votre prénom" required/>    
+                </li> 
 
-        <li>  
-            <input type="submit" value="Soumettre Formulaire">  
-        </li>  
-    </ul>  
-</form>  
-</body>  
+                <li>  
+                    <label for="mdp">Mot de passe :</label>  
+                    <input type="password" name="mdp" id="mdp" placeholder="Votre prénom" required/>    
+                </li> 
+
+                <li>  
+                    <label for="bom">Nom :</label>  
+                    <input type="text" name="nom" id="nom" placeholder="Votre nom"/> 
+                </li>  
+
+                <li>  
+                    <label for="prenom">Prénom :</label>  
+                    <input type="text" name="prenom" id="prenom" placeholder="Votre prénom" required/> 
+                </li>  
+
+                <li>  
+                    <label for="telephone">Téléphone :</label>  
+                    <input type="tel" name="telephone" id="telephone" /> 
+                </li> 
+
+                <li>  
+                    <label for="site">Site web :</label>  
+                    <input type="url" name="site" id="site" /> 
+                </li>  
+
+                <li>  
+                    <label for="sexe">Sexe :</label>  
+                    <input type="radio" name="sexe" value="Femme" /> Femme
+                    <input type="radio" name="sexe" value="Homme" /> Homme
+                </li>  
+                
+                <li>  
+                    <label for="dateNaissance">Date naissance :</label>  
+                    <input type="date" name="dateNaissance" id="dateNaissance" required /> 
+                </li> 
+
+                <li>  
+                    <label for="age">Age :</label>  
+                    <input type="number" name="age" id="age" disabled /> 
+                </li>
+
+                <li>  
+                    <label for="ville">Ville :</label>  
+                    <input type="text" name="ville" id="ville" /> 
+                </li> 
+
+                <li>  
+                    <label for="taille">Taille :</label>  
+                    <input type="range" name="taille" value="0" max="2.50" min="0" step="0.01">
+                </li> 
+
+                <li>  
+                    <label for="color">Couleur préférée :</label>  
+                    <input type="color" name="couleur" id="color" value="#000000">
+                </li> 
+
+                <li>  
+                    <label for="photo">Photo de profil :</label>  
+                    <input type="file" name="photo" id="photo" value="#000000">
+                </li> 
+
+                <li>  
+                    <input type="submit" value="Soumettre Formulaire">  
+                </li>  
+            </ul> 
+
+        </form>  
+    </body>  
 </html>  
