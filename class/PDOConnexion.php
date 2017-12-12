14cf46aeafa228d878d161fe9e80b2b35e399f76
em612865 <karmoudimohamed@gmail.com>
2017-12-12 15:22:06 +0100

    Ajout commentaire class PDO + move vers dossier class

--- class/PDOConnexion.php
+++ class/PDOConnexion.php
@@ -1,16 +1,24 @@
 <?php 
 	class PDOConnexion{
+        const DB_NAME 	= "pictionnary";
+        const LOGIN 	= "test";
+        const PASSWORD 	= "test";
+
+        // Instance de PDOConnexion
 		static private $instance = null;
 		private $pdo;
 
-		const DB_NAME 	= "pictionnary";
-		const LOGIN 	= "test";
-		const PASSWORD 	= "test";
-
+        /**
+         * L'encapsulation du construction est privé afin de limiter l'instanciation des object.
+         */
 		private function __construct(){
 			$this->pdo = new PDO('mysql:host=localhost;dbname='.self::DB_NAME, self::LOGIN, self::PASSWORD);
 		}
 
+        /**
+         * Retourne une instance de PDOConnexion.
+         * getInstance permet de controler l'instanciation de la classe PDOConnexion.
+         */
 		public static function getInstance(){
 			if(is_null(self::$instance)){
 				self::$instance = new PDOConnexion();
@@ -19,6 +27,10 @@
 			return self::$instance;
 		}
 
+        /**
+         * Execute la requete "$requete".
+         * DatasArray corresponds aux données à binder à la requete.
+         */
 		public function executer($requete, $datasArray = null){
             $req = $this->pdo->prepare($requete);
 
